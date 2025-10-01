99f3be23ab91897515c076796313065e6a4ad512
fadam430 <f.adam430@gmail.com>
2025-10-01 14:10:14 +0100

    little changees to readme

--- assets/js/test/script.test.js
+++ assets/js/test/script.test.js
@@ -3,29 +3,3 @@
  * @jest-environment jsdom
  */
 
-
-
-beforeEach(() => {
-    document.body.innerHTML = `game.html`;
-    document.body.innerHTML = `index.html`;
-});
-
-describe('open index.html and load content', () => {
-    test('should load the content of index.html', () => {
-        expect(document.body).toBeDefined();
-    });
-});
-
-describe('open game.html and load content', () => {
-    test('should load the content of game.html', () => {
-        expect(document.body).toBeDefined();
-    });
-});
-describe('openNewGame function exist', () => {
-    test('openNewGame should be a function', () => {
-        const { openNewGame } = require('../script.js');
-        expect(typeof openNewGame).toBe('function');
-    });
-});
-
-
